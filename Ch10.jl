2c24d252138bba1f1ebd67ab4b13d38161cc6253
Christian Goldammer <goldammer.christian@gmail.com>
2015-04-20 21:55:10 -0500

    Added exercise 10.1-2

--- Ch10.jl
+++ Ch10.jl
@@ -96,3 +96,46 @@ q = Queue(1)
 enqueue!(q, 1)
 @test_throws ErrorException enqueue!(q, 1)
 
+# Exercise 10.1-2
+# Implement two stacks in one array such that  neither overflows until the total
+# number of elements is bigger than n.
+
+# I achieve this by storing the data as follows [s1, ..., s2] where the ... represent potentially missing values
+# and s2 is stored in reverse order.
+
+type twostack
+  top::Array{Int64, 1}
+  data
+end
+
+twostack(n) = twostack([0, n+1], {None for i=1:n})
+
+stack_empty(ts::twostack, stack::Int64) = stack==1 ? ts.top[stack]==0 : ts.top[stack]==length(ts.data)+1
+
+function tspush!(ts::twostack, stack::Int64, x)
+  n = length(ts.data)
+  top = ts.top[stack] + (stack==1 ? 1 : -1)
+  if stack==1 ? top >= ts.top[2] : top<= ts.top[1]
+    error("Twostack overflow")
+  end
+  ts.top[stack] = top
+  ts.data[top] = x
+end
+
+function tspop!(ts::twostack, stack::Int64)
+  if stack_empty(ts, stack)
+    error("Twostack underflow")
+  else
+    ts.top[stack] = ts.top[stack] + (stack==1 ? -1 : 1)
+  end
+  stack==1 ? 1 : -1
+end
+
+stack=2
+ts = twostack(1)
+@test_throws ErrorException tspop!(ts, 1)
+@test_throws ErrorException tspop!(ts, 2)
+tspush!(ts, 1, 1)
+@test_throws ErrorException tspush!(ts, 1, 1)
+@test_throws ErrorException tspush!(ts, 2, 1)
+
